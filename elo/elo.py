6203ccb847db31eeef55c73066cdde85c64b09ba
Antonio Iniguez <antonio@myndshft.com>
2019-03-20 16:55:47 -0700

    initial push of flask server and elo calcs

---
+++ elo/elo.py
@@ -0,0 +1,27 @@
+import json
+from typing import List
+
+
+def elo_adjust(outcomes: json, current) -> List[dict]:
+    """
+    Takes in a score dictionary (must be len 2) in the format {key:value, key:value} where key is name
+    for each player and value is their score in the game
+    Ex. {"Brian": 0, "TylerW":11}
+    Ex. {"p1_name": "Brian", "p1_score": 0, "p2_name": "Tyler", "p2_score": 11}
+    """
+
+    p1 = outcomes["p1_name"]
+    p2 = outcomes["p2_name"]
+    p1_score = int(outcomes["p1_score"])
+    p2_score = int(outcomes["p2_score"])
+    total = int(max(p1_score, p2_score))
+    p1_rating = current["p1_current"]
+    p2_rating = current["p2_current"]
+    exp1 = (1 / (1 + 10 ** ((p2_rating - p1_rating) / 400))) * total
+    exp2 = (1 / (1 + 10 ** ((p1_rating - p2_rating) / 400))) * total
+    player_updates = [
+        {"name": p1, "rating": p1_rating + 16 * (p1_score - exp1)},
+        {"name": p2, "rating": p2_rating + 16 * (p2_score - exp2)},
+    ]
+    return player_updates
+
---
+++ elo/main.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+from elo import elo_adjust
+from pydbg import dbg
+
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "app.sqlite"
+)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    rating = db.Column(db.Integer, unique=False)
+
+    def __init__(self, name, rating):
+        self.name = name
+        self.rating = rating
+
+
+class PlayerSchema(ma.Schema):
+    class Meta:
+        fields = ("name", "rating")
+
+
+player_schema = PlayerSchema()
+players_schema = PlayerSchema(many=True)
+
+
+@app.route("/player", methods=["POST"])
+def add_player():
+    name = request.form["name"]
+    new_player = Player(name, 1600)
+    db.session.add(new_player)
+    db.session.commit()
+    return f"success, new player {name} create with rating 1600"
+
+
+@app.route("/player", methods=["GET"])
+def get_all():
+    all_players = Player.query.all()
+    result = players_schema.dump(all_players)
+    return jsonify(result.data)
+
+
+@app.route("/player/<n>", methods=["GET"])
+def get_player(n):
+    player = Player.query.filter_by(name=n).first_or_404()
+    result = player_schema.dump(player)
+    return jsonify(result.data)
+
+
+@app.route("/player-rating/<name>", methods=["GET"])
+def get_rating(name):
+    player = Player.query.filter_by(name=name).first_or_404()
+    result = player_schema.dump(player)
+    return jsonify(result.data["rating"])
+
+
+@app.route("/update-ratings", methods=["POST"])
+def update_rating():
+    ratings = {
+        "p1_current": get_rating(request.form["p1_name"]).get_json(),
+        "p2_current": get_rating(request.form["p2_name"]).get_json(),
+    }
+
+    new_ratings = elo_adjust(request.form, ratings)
+    for p in new_ratings:
+        player = Player.query.filter_by(name=p["name"]).first_or_404()
+        player.name = p["name"]
+        player.rating = p["rating"]
+        db.session.commit()
+    return f"Players update - {new_ratings}"
+
+
+@app.route("/remove-player/<n>", methods=["DELETE"])
+def del_player(n):
+    player = Player.query.filter_by(name=n).first_or_404()
+    db.session.delete(player)
+    db.session.commit()
+    return f"{n} removed from database"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
