abd03119d296dff2aa937f12ae81313f31439d0c
Antonio Iniguez <antonio@myndshft.com>
2019-03-20 22:28:49 -0700

    edits

--- elo/elo.py
+++ elo/elo.py
@@ -9,24 +9,23 @@ def elo_adjust(outcomes: json, current) -> List[dict]:
     Ex. {"Brian": 0, "TylerW":11}
     Ex. {"p1_name": "Brian", "p1_score": 0, "p2_name": "Tyler", "p2_score": 11}
     """
-
-    p1 = outcomes["p1_name"]
-    p2 = outcomes["p2_name"]
-    p1_score = int(outcomes["p1_score"])
-    p2_score = int(outcomes["p2_score"])
-    total = p1_score + p2_score
+    total = int(outcomes["p1_score"]) + int(outcomes["p2_score"])
     p1_rating = current["p1_current"]
     p2_rating = current["p2_current"]
     exp1 = (1 / (1 + 10 ** ((p2_rating - p1_rating) / 400))) * total
     exp2 = (1 / (1 + 10 ** ((p1_rating - p2_rating) / 400))) * total
     player_updates = [
         {
-            "name": p1,
-            "rating": int(round(max(100, p1_rating + 25 * (p1_score - exp1)), 0)),
+            "name": outcomes["p1_name"],
+            "rating": int(
+                round(max(100, p1_rating + 25 * (int(outcomes["p1_score"]) - exp1)), 0)
+            ),
         },
         {
-            "name": p2,
-            "rating": int(round(max(100, p2_rating + 25 * (p2_score - exp2)), 0)),
+            "name": outcomes["p2_name"],
+            "rating": int(
+                round(max(100, p2_rating + 25 * (int(outcomes["p2_score"]) - exp2)), 0)
+            ),
         },
     ]
     return player_updates
